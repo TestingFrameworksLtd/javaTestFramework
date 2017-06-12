7de99f1d77f2a07f5c9ad66d49857858beb4d0e4
VikasAggarwal1978 <vikas_aggarwal11@yahoo.com>
2017-06-12 11:03:13 +0100

    Read from file function

--- src/test/java/utility/FileUtils.java
+++ src/test/java/utility/FileUtils.java
@@ -4,4 +4,18 @@ package utility;
  * Created by vikas on 01/03/2017.
  */
 public class FileUtils {
+  
+   public static String readFile(String path) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String sCurrentLine = null;
+            while ((sCurrentLine = br.readLine()) != null) {
+                sb.append(sCurrentLine);
+            }
+        } catch (IOException io) {
+            throw new RuntimeException(io.getMessage());
+        }
+        return sb.toString();
+    }
 }
